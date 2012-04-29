a836fb0040ee4efbad0fdb1b7aaf5289be4e2c5a
Tony Hartzell <hartzell.tony@gmail.com>
2012-04-29 16:43:48 -0700

    New Gemfile README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppw2::Application.config.secret_token = 'eb2e6a6fee38235a7e6e854708fb681479eadd56fad5cd674d517ed8c66035f1d2d231636156644e7b8fa2b8e4940cf57b7d6cd53ff8d21e9dc535cfd29b20af'
