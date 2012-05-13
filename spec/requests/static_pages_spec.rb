cbe4a82b7cebe22013d82cfb4ec716912fc9ac66
Tony Hartzell <hartzell.tony@gmail.com>
2012-05-13 10:27:29 -0700

    Added Contact Us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+  end
 end
